9f2a33ec2622883f24bc4dd77132b3e998206089
Ivkaran Singh <ivkaransingh@ivkarans-mbp.mynetworksettings.com>
2025-02-22 21:01:44 -0500

    first commit

---
+++ src/main/java/com/jobmatrix/jm_notification/JmNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.jobmatrix.jm_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobmatrix/jm_notification/JmNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobmatrix.jm_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
